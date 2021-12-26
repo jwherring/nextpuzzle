7cd961f7cb6d1344b537eed717cb1c31ef89b77d
Joshua Herring <joshuaherring@system76-pc.localdomain>
2021-12-26 18:59:47 -0500

    handle commandline

--- dailypuzzles.c
+++ dailypuzzles.c
@@ -13,6 +13,12 @@ char const *create_puzzles_table = "create table puzzles (id int primary key, pu
 char const *create_results_table = "create table results (id int primary key, puzzle text not null, date text not null, result integer default 0)";
 char const *create_score_table = "create table score (id int primary key, puzzle text not null, score integer default 0)";
 char const *dtformat = "%F";
+char const *useage = 
+  "Useage dailypuzzles <command>\n"
+  "COMMAND\n"
+  " if command is \"next\" prints the next puzzle for the day, if available\n"
+  " if command is not \"next\" it should be a puzzle number followed by the character 's' or 'f' indicating success or failure\n";
+
 mode_t fullmode = S_IRWXU|S_IRWXG|S_IRWXO;
 
 int database_file_exists() {
@@ -123,19 +129,55 @@ char* get_today() {
 
 }
 
-int main() {
-  sqlite3* dbc = get_db_conn();
-  sqlite3_close(dbc);
-  for(int i = 0; i < 10; i++){
-    printf("[%d]: %d\n", i, fibonacci1(i));
+void print_useage() {
+  puts(useage);
+}
+
+void get_next() {
+  printf("GET NEXT\n");
+}
+
+int check_success_arg(char * success_arg) {
+  return strcmp(success_arg, "f") == 0 || strcmp(success_arg, "s") == 0;
+}
+
+void update_puzzle(char * command_arg, char * success_arg) {
+}
+
+int main(int argc, char** argv) {
+
+  char * command_arg;
+  char * success_arg;
+  if(argc < 2 || argc > 3){
+    print_useage();
+    return 0;
   }
 
-  char * today = get_today();
-  char * tomorrow = get_target_day(1);
-  char * next_month = get_target_day(30);
-  printf("TODAY: %s\n", today);
-  printf("TOMORROW: %s\n", next_month);
-  free(today);
+  command_arg = argv[1];
+
+  if(argc == 2) {
+
+    if(strcmp(command_arg, "next") != 0){
+      print_useage();
+      return 0;
+    } 
+    get_next();
+    return 0;
+  }
+
+  success_arg = argv[2];
+
+  if(!check_success_arg(success_arg)){
+    print_useage();
+    return 0;
+  }
+
+  update_puzzle(command_arg, success_arg);
+
+  sqlite3* dbc = get_db_conn();
+  sqlite3_close(dbc);
+
+
   return 0;
 
 }
